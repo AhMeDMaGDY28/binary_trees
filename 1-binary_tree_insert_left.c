d8d57e60eac59e02af6873791b131fa8934d51e1
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-03-26 07:41:03 +0200

    T1 fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,18 +4,21 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_tree;
 
+	/* NULL on failure or if parent is NULL */
 	if (!parent)
 		return (NULL);
 	new_left_tree = malloc(sizeof(binary_tree_t));
+	/* NULL on failure or if parent is NULL */
 	if (!new_left_tree)
 		return (NULL);
 
 	new_left_tree->n = value;
 	new_left_tree->parent = parent;
-	new_left_tree->left = parent->left;
+	new_left_tree->left = NULL;
 	new_left_tree->right = NULL;
-	if (parent->left)
+	if (parent->left != NULL)
 	{
+		new_left_tree->left = parent->left;
 		parent->left->parent = new_left_tree;
 	}
 	parent->left = new_left_tree;
