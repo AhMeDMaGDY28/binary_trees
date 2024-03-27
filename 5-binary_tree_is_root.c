9c8348543407169a9d48edd6c21400bb6ee6ac4e
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-03-27 06:23:08 +0200

    T5 fix 0

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -18,7 +18,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (!node)
 		return (0);
 	/* function must return 1 if node is a root, otherwise 0*/
-	if (!node->parent && node->left && node->right)
+	if (!node->parent && node)
 		return (1);
 	return (0);
 }
