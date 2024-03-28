3e1f88a6d057cd368a2821e0fe51c68e67748e10
karxg <karim4420@aol.com>
2024-03-28 11:31:55 +0200

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@
  * Return: The number of nodes in the binary tree. If the tree
  *         is NULL or has no nodes, the function returns 0.
  *
- * Author: AhMeDMaGDY28
+ * Author: AhMeDMaGDY28 & karxg
  * School: ALX CO 1 BLENDED
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
