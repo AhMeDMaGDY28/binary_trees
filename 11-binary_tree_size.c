27b02fd25c8a6a0c52fc078afb9ceff3f9c3e98e
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-03-28 06:50:51 +0200

    T11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,17 +1,26 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_size - Measures the size of a binary tree.
-* @tree: A pointer to the root node of the tree.
-*
-* This function calculates the total number of nodes in a binary tree,
-* including the root node and all of its children.
-*
-* Return: The size of the tree.
-*         0 if @tree is NULL.
-*/
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * This function measures the size of a binary tree, i.e., the number of nodes
+ * in the tree. If the tree is NULL, the size is considered to be 0.
+ *
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * Return: The size of the binary tree. If the tree is NULL, the function
+ *         returns 0.
+ *
+ * Author: AhMeDMaGDY28
+ * School: ALX CO 1 BLENDED
+ */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
